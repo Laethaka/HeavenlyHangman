406faacfef03d67a30ffb8633b6cc99e2b339780
Jesse <laethaka@gmail.com>
2018-05-22 16:37:28 -0700

    wordJS works

--- word.js
+++ word.js
@@ -2,9 +2,12 @@ var Letter = require('./letter.js');
 
 var Word = function(secretWord) {
     this.letterArr = [];
-    while (this.letterArr.length < secretWord.length) {
-        this.letterArr.push(new Letter('x'))
-    };
+    for (let i=0; i < secretWord.length; i++) {
+        this.letterArr.push(new Letter(secretWord[i]))
+    }
+    // while (this.letterArr.length < secretWord.length) {
+    //     this.letterArr.push(new Letter('x'))
+    // };
     this.displayWord = function() {
         var displayStr = '';
         for (let i=0; i < this.letterArr.length; i++) {
@@ -19,5 +22,6 @@ var Word = function(secretWord) {
 };
 
 // var myWord = new Word('phil')
+// console.log(myWord)
 
 module.exports = Word;
